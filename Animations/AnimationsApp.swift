19142012c506d03151d24dc88a1e1bb8a178031a
Muhammad Aarfeen <74659552+MuhammadAarfeen@users.noreply.github.com>
2022-07-30 03:47:51 +0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Muhammad Aarfeen on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
